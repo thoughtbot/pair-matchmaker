ef8cafc70675b916dc3fad3068a52937473c497a
Richard Newman <114449566+rdnewmanbot@users.noreply.github.com>
2025-10-22 14:28:38 -0600

    upgrade to Rails 8.0.3

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module DoubleUp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -35,6 +35,7 @@ module DoubleUp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_support.to_time_preserves_timezone = :zone
 
     # Don't generate system test files.
     config.generators.system_tests = nil
