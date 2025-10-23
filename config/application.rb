ad60fa537f568979c83d965aa2559479fd921169
Richard Newman <richard.newman@thoughtbot.com>
2025-10-23 10:23:58 -0600

    Update to Rails 8.0.3 (#13)

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,18 @@
 require_relative "boot"
 
 require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
 require "active_job/railtie"
-require "action_controller/railtie"
 require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,7 +21,12 @@ Bundler.require(*Rails.groups)
 module DoubleUp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 8.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -22,6 +35,7 @@ module DoubleUp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_support.to_time_preserves_timezone = :zone
 
     # Don't generate system test files.
     config.generators.system_tests = nil
